bb2e128199f376909ce82d75fc5d77acd17d20e4
largomst <dingrui.zhen@gmail.com>
2021-06-18 18:27:47 +0800

    add withdraw api

--- bankapi/main.go
+++ bankapi/main.go
@@ -8,10 +8,10 @@ import (
 	"strconv"
 )
 
-var accounts = map[float64]bank.Account{}
+var accounts = map[float64]*bank.Account{}
 
 func main() {
-	accounts[1001] = bank.Account{
+	accounts[1001] = &bank.Account{
 		Customer: bank.Customer{
 			Name:    "John",
 			Address: "Los Angles, California",
@@ -20,6 +20,7 @@ func main() {
 	}
 	http.HandleFunc("/statement", statement)
 	http.HandleFunc("/desposit", deposit)
+	http.HandleFunc("/withdraw", withdraw)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -60,6 +61,34 @@ func deposit(w http.ResponseWriter, req *http.Request) {
 				fmt.Fprintf(w, "Account with number %v can't be found!", number)
 			} else {
 				err := account.Deposit(amount)
+				if err != nil {
+					fmt.Fprintf(w, "%v", err)
+				} else {
+					fmt.Fprint(w, account.Statement())
+				}
+			}
+		}
+	}
+}
+
+func withdraw(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	} else {
+		if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+			fmt.Fprintf(w, "Invalid account number!")
+		} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+			fmt.Fprintf(w, "Invalid amount number!")
+		} else {
+			account, ok := accounts[number]
+			if !ok {
+				fmt.Fprintf(w, "Account with number %v can't be found!", number)
+			} else {
+				err := account.Withdraw(amount)
 				if err != nil {
 					fmt.Fprintf(w, "%v", err)
 				} else {

3fb2a36f44a8324616dc1756fd2410e701fc12b5
largomst <dingrui.zhen@gmail.com>
2021-09-26 22:53:55 +0800

    update main

--- go-by-example/main.go
+++ go-by-example/main.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -511,6 +513,7 @@ func main() {
 	}
 	close(requests)
 
+	// 速率限制
 	limiter := time.Tick(200 * time.Millisecond)
 
 	for req := range requests {
@@ -537,13 +540,14 @@ func main() {
 		fmt.Println("request", req, time.Now())
 	}
 
+	// 原子操作计数器
 	var ops uint64
 	var wg2 sync.WaitGroup
 	for i := 0; i < 50; i++ {
 		wg2.Add(1)
 		go func() {
 			for c := 0; c < 1000; c++ {
-				// atomic.AddUint64(&ops, 1) //原子操作，避免竞争
+				atomic.AddUint64(&ops, 1) //原子操作，避免竞争
 				// ops += 1 //go run -race . 可以查看竞争详情
 			}
 			wg2.Done()
@@ -552,6 +556,47 @@ func main() {
 	wg2.Wait()
 	fmt.Println("ops", ops)
 
+	// 互斥锁
+	var mutex sync.Mutex
+	var state = make(map[int]int) // 用于读写的变量
+	var (
+		readOps  uint64 // 统计读的次数
+		writeOps uint64 // 统计写的次数
+	)
+	for i := 0; i < 100; i++ {
+		go func() {
+			total := 0
+			for {
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(1 * time.Millisecond)
+			}
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		go func() {
+			for {
+				key := rand.Intn(5)
+				val := rand.Intn(100)
+				mutex.Lock()
+				state[key] = val
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(1 * time.Millisecond)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+	readOpsFinal := atomic.LoadUint64(&readOps) // 读操作的总次数
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps) // 写操作的总次数
+	fmt.Println("writeOps:", writeOpsFinal)
+	mutex.Lock()
+	fmt.Println("state:", state) // 读取结果
+	mutex.Unlock()
 }
 
 type (
